8dd843b367e7c92b74a5e4e934a8ec5f1a890b95
martycook <martinc@valornet.com>
2020-10-30 12:26:20 -0500

    refactoring

---
+++ src/CG.Serilog/Options/SerilogServiceOptions.cs
@@ -0,0 +1,13 @@
+﻿using CG.Options;
+using System;
+
+namespace CG.Serilog.Options
+{
+    /// <summary>
+    /// This class represents configuration options for Serilog based logging.
+    /// </summary>
+    public class SerilogServiceOptions : OptionsBase
+    {
+
+    }
+}
